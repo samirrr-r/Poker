2b30fcc4fb71266dfba02c05bbbcd227b4b74369
Samir Abdosh <samirabdosh1@gmail.com>
2024-03-21 11:18:05 -0500

    Added test for the different rankings in the Hand class

--- spec/Poker_spec.rb
+++ spec/Poker_spec.rb
@@ -54,4 +54,36 @@ RSpec.describe Hand do
 
   let(:royal_hand) {Hand.new(Card.new("Hearts", 1), Card.new("Hearts", 10), Card.new("Hearts", 11),
   Card.new("Hearts", 12) ,Card.new("Hearts", 13))}
+  context "Different Hand Rankings" do
+    it "returns true when there is a flush" do
+      expect(flush_hand.is_flush?).to eq(true)
+    end
+    it "returns true if the hand is a pair" do
+      expect(flush_hand.is_pair?).to eq(true)
+    end
+    it "returns true if the hand is a two pair" do
+      expect(two_pair_hand.is_two_pair?).to eq(true)
+    end
+    it "returns true if the hand is a three of a kind" do
+      expect(three_pair_hand.is_three_of_kind?).to eq(true)
+    end
+    it "returns true if the hand is a straight" do
+      expect(straight_hand.is_straight?).to eq(true)
+    end
+    it "returns true if the hand is a full house" do
+      expect(full_hand.is_full_house?).to eq(true)
+    end
+    it "returns true if the hand is a four of a kind" do
+      expect(four_hand.is_four_kind?).to eq(true)
+    end
+    it "returns if a hand is a straight flush" do
+      expect(straight_hand.is_straight_flush?).to eq(true)
+    end
+    it "returns if a hand is a royal flush" do
+      expect(royal_hand.is_roayl_flush?).to eq(true)
+    end
+    it "returns the high hand" do
+      expect(straight_hand.high_card).to eq(10)
+    end
+  end
 end
